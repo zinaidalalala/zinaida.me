b5a63807bb53f0b4eeb007a0eb068cabf24c8cc0
Zinaida Danelian <143716592+zinaidalalala@users.noreply.github.com>
2024-04-23 21:21:36 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function typeWriter(element, text, delay = 100) {
+  let i = 0;
+  let timer = setInterval(function () {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, delay);
+}
+
+let element = document.querySelector('.main-title');
+let text = 'Hi 👋 I am Zinaida, Frontend Developer';
+
+typeWriter(element, text);
